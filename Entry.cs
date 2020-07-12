d76565e62e4bb7fe632e9ba89d83a87c3f069dcd
Jacob Archambault <jacob.archambault@waystar.com>
2020-07-12 16:57:39 -0400

    Add entry class to project.

---
+++ Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsAddressBook
+{
+    // An entry object with name, address, and phone number string fields.
+    internal class Entry
+    {
+        internal string Name { get; set; }
+        internal string EmailAddress { get; set; }
+        internal string PhoneNumber { get; set; }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -20,7 +21,6 @@ namespace WinFormsAddressBook
 
         private void addEntryButton_Click(object sender, EventArgs e)
         {
-
         }
 
         private void nameTextBox_Validating(object sender, CancelEventArgs e)
